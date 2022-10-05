f71dfa6f33e47f1b36f589f2748535550b2ec3d7
Timon Kruiper <timonkruiper@gmail.com>
2022-10-05 19:35:29 +0200

    Kernel/aarch64: Add implementation of Processor::is_bootstrap_processor
    This should really go into a generic Processor class, but there is no
    such class yet, so a FIXME is added for that.

--- Kernel/Arch/aarch64/Processor.h
+++ Kernel/Arch/aarch64/Processor.h
@@ -158,6 +158,12 @@ public:
         return *g_current_processor;
     }
 
+    // FIXME: Move this into generic Processor class, when there is such a class.
+    ALWAYS_INLINE static bool is_bootstrap_processor()
+    {
+        return Processor::current_id() == 0;
+    }
+
     static void deferred_call_queue(Function<void()> /* callback */)
     {
         VERIFY_NOT_REACHED();

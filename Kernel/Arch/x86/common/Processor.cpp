7f58fa97dda7e3af7922484cb66774a4cf8afa11
Timon Kruiper <timonkruiper@gmail.com>
2022-10-25 22:17:57 +0200

    Kernel: Move ScopedCritical.cpp to Kernel base directory
    This file does not contain any architecture specific implementations,
    so we can move it to the Kernel base directory. Also update the relevant
    include paths.

--- Kernel/Arch/x86/common/Processor.cpp
+++ Kernel/Arch/x86/common/Processor.cpp
@@ -23,11 +23,11 @@
 #include <Kernel/Arch/Interrupts.h>
 #include <Kernel/Arch/Processor.h>
 #include <Kernel/Arch/SafeMem.h>
-#include <Kernel/Arch/ScopedCritical.h>
 #include <Kernel/Arch/TrapFrame.h>
 #include <Kernel/Arch/x86/CPUID.h>
 #include <Kernel/Arch/x86/MSR.h>
 #include <Kernel/Arch/x86/ProcessorInfo.h>
+#include <Kernel/ScopedCritical.h>
 
 #include <Kernel/Memory/PageDirectory.h>
 #include <Kernel/Memory/ScopedAddressSpaceSwitcher.h>
